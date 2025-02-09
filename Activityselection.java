329923c18fa524a889bf6c572bea322c6d13a3b4
Shruti Tiwari <167226355+Shruti0534@users.noreply.github.com>
2025-02-09 15:11:17 +0530

    Create Activityselection.java
    you are given n activities with their start and end time . select maximum number of activities that can be person performance and work one at a time , keep in mind that end time are not sorted

---
+++ Activityselection.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Activityselection{
+  public static void main(String args[]){
+    int start[]={};
+    int end[]={};
+    int activities[][]=new int[start.length][3];
+    for(int i=0;i<start.length;i++){
+      activities[i][0]=i;
+      activities[i][1]=start[i];
+      activities[i][2]=end[i];
+    }
+    //lambda function
+  Arrays.sort(activities, Comparator.comparingDouble(o->o[2]);
+    int maxAct=0;
+    Arraylist<Integer>ans= new ArrayList<>():
+      int maxAct=1;
+    ans.add(activities[0][0]0);
+    int lastEnd=activities[0][2];
+    fot(int i=0;i,start.length;i++){
+      if(activities[i][1]>=lastEnd){
+        maxAct++;
+        ans.add(activities[i][0]);
+        lastEnd=activities[i][2];
+      }
+    }
+    System.out.print(maxAct);
